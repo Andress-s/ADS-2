a3bff5cc1e74258e5d0f5a71c310e692e0ba9a70
Andresss <53404779+Andress-s@users.noreply.github.com>
2023-03-03 10:32:20 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,14 +2,12 @@
 #include <cstdint>
 #include "alg.h"
 
-double pown(double value, std::int64_t n) {
-    if (n == 0)
-        return 1;
-    double result = 1;
-    for (int i = 0; i < n; i++) {
-        result *= value;
-    }
-    return result;
+double pown(double value, uint16_t n) {
+  double result = 1;
+  for (int i = 0; i < n; i++) {
+    result *= value;
+  }
+  return result;
 }
 
 uint64_t fact(uint16_t n) {
@@ -20,37 +18,34 @@ uint64_t fact(uint16_t n) {
   }
 }
 
-double calcItem(double x, std::int16_t n) {
-    double sum = 0;
-    sum += (pown(x, n) / fact(n));
-    return sum;
+double calcItem(double x, uint16_t n) {
+  double sum = 0;
+  sum += (pown(x, n) / fact(n));
+  return sum;
 }
 
-double expn(double x, std::int16_t count) {
-    double ex = 1, p = 1, f = 1;
-    for (int i = 0; i < count; i++) {
-        ex += (pown(x, p) / fact(f));
-        p++;
-        f++;
-    }
-    return ex;
+double expn(double x, uint16_t count) {
+  double ex = 1, p = 1, f = 1;
+  for (int i = 0; i < count; i++) {
+    ex += (pown(x, p) / fact(f));
+    p++;
+    f++;
+  }
+  return ex;
 }
 
-double sinn(double x, std::int16_t count) {
-    double result = 0;
-    int n = 1;
-    for (int i = 0; i < count; i++) {
-        result += pown(-1, n - 1) * (pown(x, 2 * n - 1) / fact(2 * n - 1));
-        n++;
-    }
-    return result;
+double sinn(double x, uint16_t count) {
+  double result = 0, n = 1;
+  for (int i = 0; i < count; i++) {
+    result += pown(-1, n - 1) * (pown(x, 2 * n - 1) / fact(2 * n - 1));
+  }
+  return result;
 }
 
-double cosn(double x, std::int16_t count) {
-    double result = 0, n = 1;
-    for (int i = 0; i < count; i++) {
-        result += pown(-1, n - 1) * (pown(x, 2 * n - 2) / fact(2 * n - 2));
-        n++;
-    }
-    return result;
+double cosn(double x, uint16_t count) {
+  double result = 0, n = 1;
+  for (int i = 0; i < count; i++) {
+    result += pown(-1, n - 1) * (pown(x, 2 * n - 2) / fact(2 * n - 2));
+  }
+  return result;
 }
